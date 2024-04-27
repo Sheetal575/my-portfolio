f5f1ebc537dd4ef3dc26039a343a36797b9957d6
Sheetal Dadhich <56470040+Sheetal575@users.noreply.github.com>
2024-04-27 06:30:23 +0000

    responsive design

--- src/app/features/contact/contact.tsx
+++ src/app/features/contact/contact.tsx
@@ -1,10 +1,14 @@
 import styles from "./contact.module.scss"
 export const Contact = () => {
+
     return (
         <div className={styles.contact}>
             <div>
                 <div>Let&apos;s get in touch?</div>
-                <button className={styles.download_button}>Download Resume</button>
+                <div className={styles.contact__button}>
+                    <button className={styles.download_button}>Download Resume</button>
+                    <button className={styles.email_button}>sheetaldadh9@gmail.com</button>
+                </div>
             </div>
         </div>
     )
