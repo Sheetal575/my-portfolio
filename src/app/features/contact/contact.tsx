1ae376ad416a5d407b3304f275fb63ac12834fa8
Sheetal575 <sheetaldadh9@gmail.com>
2024-08-17 19:32:25 +0530

    download resume

--- src/app/features/contact/contact.tsx
+++ src/app/features/contact/contact.tsx
@@ -1,15 +1,25 @@
-import styles from "./contact.module.scss"
+import styles from "./contact.module.scss";
 export const Contact = () => {
+  const handleDownloadResume = () => {
+    window.open("https://shorturl.at/6Nqha", "_blank");
+  };
 
-    return (
-        <div className={styles.contact}>
-            <div>
-                <div>Let&apos;s get in touch?</div>
-                <div className={styles.contact__button}>
-                    <button className={styles.download_button}>Download Resume</button>
-                    <button className={styles.email_button}>sheetaldadh9@gmail.com</button>
-                </div>
-            </div>
+  return (
+    <div className={styles.contact}>
+      <div>
+        <div>Let&apos;s get in touch?</div>
+        <div className={styles.contact__button}>
+          <button
+            className={styles.download_button}
+            onClick={handleDownloadResume}
+          >
+            Download Resume
+          </button>
+          <button className={styles.email_button}>
+            sheetaldadh9@gmail.com
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
