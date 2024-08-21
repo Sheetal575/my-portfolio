df762ad82590ff9ec8dfc7fc40600fa34645701a
Sheetal Dadhich <56470040+Sheetal575@users.noreply.github.com>
2024-08-21 18:47:12 +0000

    added height and width to the image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <title>Sheetal Dadhich | Software Developer</title>
+        <title>Sheetal Dadhich</title>
         <meta name="description" content="Software developer with expertise in modern web technologies, focusing on creating high-performance and user-friendly applications." />
         <meta name="keywords" content="Sheetal Dadhich, software developer, web development, frontend developer, modern web technologies, high-performance applications" />
 
@@ -21,11 +21,14 @@ export default function RootLayout({
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://shetal.me" />
         <meta property="og:image" content="https://shetal.me/link-preview.png" />
+        <meta property="og:image" content="https://example.com/your-preview-image.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Sheetal Dadhich | Software Developer" />
         <meta name="twitter:description" content="Software developer with expertise in modern web technologies, focusing on creating high-performance and user-friendly applications." />
-        <meta name="twitter:image:src" content="https://shetal.me/link-preview.png" />
+        <meta name="twitter:image" content="https://shetal.me/link-preview.png" />
       </Head>
       <body className={inter.className}>
         {children}
