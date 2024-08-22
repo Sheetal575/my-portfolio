0491e0f08fb6a8dcf095ea760daddc51a6e391d6
Sheetal Dadhich <56470040+Sheetal575@users.noreply.github.com>
2024-08-22 20:27:44 +0000

    footer ui change

--- src/features/contact/contact.tsx
+++ src/features/contact/contact.tsx
@@ -7,18 +7,16 @@ export const Contact = () => {
   return (
     <div className={styles.contact}>
       <div>
-        <div>Let&apos;s get in touch?</div>
-        <div className={styles.contact__button}>
-          <button
-            className={styles.download_button}
-            onClick={handleDownloadResume}
-          >
-            Download Resume
-          </button>
-          <button className={styles.email_button}>
-            sheetaldadh9@gmail.com
-          </button>
-        </div>
+        <div>Let&apos;s get in touch : )</div>
+        <div className={styles['contact-email']}>sheetaldadh9@gmail.com</div>
+      </div>
+      <div className={styles.contact__button}>
+        <button
+          className={styles.download_button}
+          onClick={handleDownloadResume}
+        >
+          Download Resume
+        </button>
       </div>
     </div>
   );
